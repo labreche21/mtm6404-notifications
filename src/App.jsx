e1e3cbc6cf86e9cd9b16267c31c7e6280ffc1e74
labreche21 <labr0157@algonquinlive.com>
2026-03-19 19:00:30 -0400

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,120 @@
-
-import { useState } from "react"
-import notificationsData from "./notifications"
-import NotificationList from "./components/NotificationList"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
 
 function App() {
-
-  const [notifications, setNotifications] = useState(notificationsData)
-
-  const clearNotification = (id) => {
-    setNotifications(
-      notifications.filter((notification) => notification.id !== id)
-    )
-  }
-
-  const clearAll = () => {
-    setNotifications([])
-  }
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="container mt-5">
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
 
-      <h1 className="mb-3">
-        Notifications ({notifications.length})
-      </h1>
+      <div className="ticks"></div>
 
-      <button 
-        className="btn btn-danger mb-3"
-        onClick={clearAll}
-      >
-        Clear All
-      </button>
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
 
-      <NotificationList>
-        {notifications.map((notification) => (
-          <div key={notification.id} className="card mb-3">
-            <div className="card-body">
-
-              <h5>{notification.name}</h5>
-              <p>{notification.message}</p>
-
-              <button
-                className="btn btn-sm btn-outline-secondary"
-                onClick={() => clearNotification(notification.id)}
-              >
-                Clear
-              </button>
-
-            </div>
-          </div>
-        ))}
-      </NotificationList>
-
-    </div>
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
   )
 }
 
